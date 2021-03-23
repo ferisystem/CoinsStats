c9063b342334be6e5360c0ff035d316f2db33e26
Alireza .Feri <16233160+ferisystem@users.noreply.github.com>
2021-03-24 01:00:37 +0430

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,11 @@ API_ID = 'ENTER API_ID'
 CMP_API_KEY = 'ENTER CoinMarketCap API'
 CHAT_ID = 'ENTER DESTINATION_ID'
 SUDO_ID = 'ENTER SUDO_ID'
-coins_list = ()
+coins_list = (
+'bitcoin', 'ethereum', 'bitcoin-cash',
+'litecoin', 'dash', 'binance-coin',
+'cardano', 'vechain', 'ontology', 'holo',
+'elrond-erd-2', 'chainlink', 'the-sandbox',
+'cosmos', '1inch', 'polkadot',
+'ripple', 'dogecoin', 'bridge-oracle'
+)
