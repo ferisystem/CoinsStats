346f680c53b10012599c388767238fc8e80bbf5d
Alireza .Feri <16233160+ferisystem@users.noreply.github.com>
2021-03-28 04:58:29 +0430

    add important values & some def

--- bot.py
+++ bot.py
@@ -3,3 +3,24 @@ from datetime import datetime
 import requests
 import asyncio
 import redis
+from config import *
+
+
+sudoID = int(SUDO_ID)
+redis = redis.Redis(host = 'localhost', port = 6379, db = 0, decode_responses = True, encoding = 'utf-8')
+client = TelegramClient('coinstats', int(API_ID), API_HASH)
+loop = asyncio.get_event_loop()
+
+
+def return_emoji_change(price_old, price):
+	if price_old > price:
+		emoji_change = "🔻"
+	elif price_old < price:
+		emoji_change = "🔹"
+	elif price == 0:
+		emoji_change = ""
+	elif price_old == price:
+		emoji_change = "🔸"
+	return emoji_chang
+
+
